b64e7c63132df6873db842e4659c2226f18f76fe
doumeng <970080063@qq.com>
2019-03-04 20:45:12 +0800

    没网络页面完成

---
+++ miniprogram/pages/unnet/unnet.js
@@ -0,0 +1,20 @@
+//获取应用实例
+var app = getApp();
+Page({
+
+	/**
+	 * 去往主页面
+	 */
+	goToIndex: function () {
+		if (app.globalData.isConnected) {
+			wx.switchTab({
+				url: '/pages/index/index',
+			});
+		} else {
+			wx.showToast({
+				title: '没网呀梦佳',
+				icon: 'none',
+			})
+		}
+	},
+})
